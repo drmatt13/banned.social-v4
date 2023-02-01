472544edc6272d4fd4ecb91c6919efd0a90398d1
Matthew Sweeney <Matthew.Sweeney001@gmail.com>
2023-02-01 04:27:55 -0500

    2/1/23

--- src/components/modal components/UploadImage.tsx
+++ src/components/modal components/UploadImage.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable @next/next/no-img-element */
 import {
   useState,
+  useLayoutEffect,
   useCallback,
   useEffect,
   useRef,
@@ -8,8 +9,14 @@ import {
   ImgHTMLAttributes,
 } from "react";
 
+import Resizer from "react-image-file-resizer";
+import isBase64 from "is-base64";
+
 // context
-// import { useGlobalContext } from "@/context/globalContext";
+import { useGlobalContext } from "@/context/globalContext";
+
+// libaries
+import { processService, serviceError } from "@/lib/processService";
 
 const UploadImage = ({
   image,
@@ -22,28 +29,97 @@ const UploadImage = ({
   loading: boolean;
   setLoading: (loading: boolean) => void;
 }) => {
-  // const { mobile } = useGlobalContext();
+  const { user, setUser, setModal, logout } = useGlobalContext();
 
   const inputRef = useRef<HTMLInputElement>(null);
   const dropRef = useRef<HTMLDivElement>(null);
   const [preview, setPreview] = useState<string | undefined>(undefined);
+  const [initialLoad, setInitialLoad] = useState(true);
 
   const assessFile = useCallback(
     (file: File | undefined | null) => {
-      if (file?.type.match(/image.*/)) {
+      if (file?.type === "image/png" || file?.type === "image/jpeg") {
         setImage(file);
       }
     },
     [setImage]
   );
 
-  const processImage = useCallback((image: File) => {
-    const reader = new FileReader();
-    reader.onload = (e) => {
-      setPreview(e.target?.result as string);
-    };
-    reader.readAsDataURL(image);
-  }, []);
+  const resizeFile = useCallback(
+    (file: File) =>
+      new Promise<string | Blob | File | ProgressEvent<FileReader>>(
+        (resolve) => {
+          Resizer.imageFileResizer(
+            file,
+            960,
+            960,
+            "JPEG",
+            80,
+            0,
+            (uri) => {
+              resolve(uri);
+            },
+            "base64"
+          );
+        }
+      ),
+    []
+  );
+
+  const uploadImage = useCallback(
+    async (e: any) => {
+      e.preventDefault();
+      if (!image) return;
+      setLoading(true);
+      const base64 = await resizeFile(image);
+      if (
+        typeof base64 !== "string" ||
+        !isBase64(base64, { mimeRequired: true })
+      ) {
+        alert("Error resizing image");
+        setLoading(false);
+        return;
+      }
+      try {
+        const data = await processService("update avatar", {
+          avatar: base64,
+        });
+        const { user, success, error } = data;
+        if (success && user) {
+          setUser(user);
+          setModal(undefined);
+        } else {
+          if (error === serviceError.Unauthorized) {
+            throw new Error(serviceError.Unauthorized);
+          } else if (error === serviceError.FailedToUpdateUser) {
+            throw new Error(serviceError.FailedToUpdateUser);
+          } else {
+            throw new Error(serviceError.ServerError);
+          }
+        }
+      } catch (error) {
+        alert("Server error");
+        logout();
+      }
+    },
+    [image, logout, resizeFile, setLoading, setModal, setUser]
+  );
+
+  const processImage = useCallback(
+    (image: File) => {
+      const reader = new FileReader();
+      reader.onload = (e) => {
+        setPreview(e.target?.result as string);
+      };
+      reader.onerror = (e) => {
+        setPreview(undefined);
+        setImage(undefined);
+        alert("Error loading image");
+      };
+      reader.readAsDataURL(image);
+    },
+    [setImage]
+  );
 
   useEffect(() => {
     if (image) {
@@ -60,9 +136,14 @@ const UploadImage = ({
     if (!dropRef.current || !inputRef.current) return;
     const dropContainer = dropRef.current;
     const input = inputRef.current;
-    const dropContainerCallback = (e: DragEvent) =>
+    const dropContainerCallback = (e: DragEvent) => {
+      if (loading) return;
       assessFile(e.dataTransfer?.files[0]);
-    const inputCallback = () => assessFile(input.files && input.files[0]);
+    };
+    const inputCallback = () => {
+      if (loading) return;
+      assessFile(input.files && input.files[0]);
+    };
     const preventDefault = (e: DragEvent) => {
       e.preventDefault();
       e.stopPropagation();
@@ -82,7 +163,13 @@ const UploadImage = ({
       dropContainer.removeEventListener("drop", dropContainerCallback, false);
       input.onchange = null;
     };
-  }, [assessFile]);
+  }, [assessFile, loading]);
+
+  useLayoutEffect(() => {
+    if (initialLoad) {
+      setInitialLoad(false);
+    }
+  }, [initialLoad]);
 
   return (
     <div className="h-full flex flex-col">
@@ -90,19 +177,23 @@ const UploadImage = ({
         <div
           className={`${
             !preview && "px-8 flex-col items-center justify-evenly flex-1"
-          } flex my-8`}
+          } ${initialLoad && "hidden"} flex my-8`}
         >
           <div
             ref={dropRef}
             className={`${
               preview
                 ? "flex-1 items-end"
-                : "justify-evenly items-center border-dashed border-2 aspect-video bg-gray-200 dark:bg-white/30"
+                : "justify-evenly items-center border-dashed border-2 aspect-video bg-gray-200 dark:bg-white/30 "
             } flex flex-col max-w-full h-full border-blue-400 dark:border-blue-600 rounded-lg`}
             onTouchEnd={() => !preview && inputRef.current?.click()}
           >
             {preview ? (
-              <div className="bg-blue-500 dark:bg-blue-600 p-1 rounded-lg mx-2 aspect-square">
+              <div
+                className={`${
+                  loading && "grayscale-[.75] cursor-not-allowed"
+                } bg-blue-500 dark:bg-blue-600 p-1 rounded-lg mx-2 aspect-square`}
+              >
                 <img
                   src={
                     preview as unknown as DetailedHTMLProps<
@@ -111,7 +202,12 @@ const UploadImage = ({
                     >["src"]
                   }
                   alt="preview"
-                  className="aspect-square w-48 /h-48 object-cover rounded-md bg-white"
+                  className={`aspect-square w-48 object-cover object-center rounded-md bg-white`}
+                  onError={() => {
+                    setPreview(undefined);
+                    setImage(undefined);
+                    alert("Error loading image");
+                  }}
                 />
               </div>
             ) : (
@@ -128,34 +224,36 @@ const UploadImage = ({
               className={`${
                 preview
                   ? "flex flex-col flex-1 mx-2 relative"
-                  : "w-64 mt-4 flex items-center justify-end"
+                  : "w-48 xs:w-64 mt-4 flex items-center justify-end"
               }`}
             >
-              <div className={`${preview ? "w-full absolute bottom-0" : ""}`}>
-                <div
+              <div className={`${preview && "w-full absolute bottom-0"}`}>
+                <button
                   className={`${
                     loading || false
                       ? "border-light-border dark:border-dark-border bg-light-secondary dark:bg-dark-accent text-gray-600 dark:text-gray-300 cursor-not-allowed"
                       : "bg-blue-500 dark:bg-blue-600 text-white hover:bg-blue-600 dark:hover:bg-blue-500 cursor-pointer"
                   } ${
                     preview ? "rounded-full flex-1" : "rounded-lg translate-x-1"
-                  } px-4 py-2 text-center select-none`}
+                  } px-4 py-2 text-center select-none w-full text-xs xs:text-sm truncate`}
                   onClick={() => inputRef.current?.click()}
+                  disabled={loading}
                 >
                   Select Image
-                </div>
-                <div
+                </button>
+                <button
                   className={`${
                     loading || false
                       ? "border-light-border dark:border-dark-border bg-light-secondary dark:bg-dark-accent text-gray-600 dark:text-gray-300 cursor-not-allowed"
-                      : "bg-red-700/90 dark:bg-red-800/75 text-white hover:bg-red-600/90 dark:hover:bg-red-600/80 cursor-pointer"
+                      : "bg-red-700/90 dark:bg-red-700/80 text-white hover:bg-red-600/90 dark:hover:bg-red-600/80 cursor-pointer"
                   } ${
                     preview ? "rounded-full flex-1" : " hidden"
-                  } px-4 py-2 text-center mt-2 select-none`}
+                  } px-4 py-2 text-center mt-2 select-none w-full text-xs xs:text-sm truncate`}
                   onClick={() => setImage(undefined)}
+                  disabled={loading}
                 >
                   Remove Image
-                </div>
+                </button>
               </div>
             </div>
           </div>
@@ -167,12 +265,17 @@ const UploadImage = ({
             ? "border-light-border dark:border-dark-border bg-light-secondary dark:bg-dark-accent text-gray-600 dark:text-gray-300 cursor-not-allowed"
             : "bg-blue-500 dark:bg-blue-600 text-white hover:bg-blue-600 dark:hover:bg-blue-500 cursor-pointer"
         }  mx-2 mb-2 py-2 rounded-full select-none`}
-        // onClick={() => inputRef.current?.click()}
+        onClick={uploadImage}
         disabled={loading || !image}
       >
         Upload Image
       </button>
-      <input ref={inputRef} type="file" accept="image/*" className="hidden" />
+      <input
+        ref={inputRef}
+        type="file"
+        accept="image/png, image/jpeg"
+        className="hidden"
+      />
     </div>
   );
 };
--- src/pages/api/services/user/update_avatar.ts
+++ src/pages/api/services/user/update_avatar.ts
@@ -1,6 +1,10 @@
 import { serviceError } from "@/lib/processService";
 import type ServiceRequest from "@/types/serviceRequest";
 import type { NextApiResponse } from "next";
+import isBase64 from "is-base64";
+
+// aws
+import AWS from "aws-sdk";
 
 // mongoose
 import connectDB from "@/lib/connectDB";
@@ -11,6 +15,29 @@ export default connectDB(async (req: ServiceRequest, res: NextApiResponse) => {
     let { _id, avatar, eventbusSecret } = req.body;
     if (eventbusSecret !== process.env.EVENTBUS_SECRET) {
       throw new Error(serviceError.Unauthorized);
+    }
+    if (isBase64(avatar || "", { mimeRequired: true })) {
+      AWS.config.update({ region: "us-east-1", apiVersion: "2006-03-01" });
+
+      const s3 = new AWS.S3({
+        accessKeyId: process.env.AWS_ACCESS_KEY,
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+      });
+
+      const params = {
+        Bucket: "social-media-8434-1348-6435",
+        Key: `${_id}`,
+        Body: Buffer.from(
+          avatar!.replace(/^data:image\/\w+;base64,/, ""),
+          "base64"
+        ),
+        ContentEncoding: "base64",
+        ContentType: "image/jpeg",
+      };
+
+      const upload = await s3.upload(params).promise();
+      avatar = upload.Location;
+      console.log("upload", upload);
     }
     const user: IUserModel | null = await UserModel.findOneAndUpdate(
       { _id },
